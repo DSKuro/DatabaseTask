6a85f3b6cb93a94b1d8173c7e70a17b95a296034
ALEX-PC\alexd <alex.dutikovsamp@yandex.ru>
2025-06-24 21:10:24 +0300

    Добавьте файлы проекта.

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace DatabaseTask.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}

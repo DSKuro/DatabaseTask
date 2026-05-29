378b7b2609b5eb49ed54ffffeaaf54fecc1da0af
DSKuro <124364432+DSKuro@users.noreply.github.com>
2026-05-29 13:13:51 +0300

    fix: fix bug with validate changes

--- ViewModels/MainViewModel/MainSubViewModels/CommandsViewModels/Utils/ValidateViewModel.cs
+++ ViewModels/MainViewModel/MainSubViewModels/CommandsViewModels/Utils/ValidateViewModel.cs
@@ -50,7 +50,7 @@ namespace DatabaseTask.ViewModels.MainViewModel.MainSubViewModels.CommandsViewMo
                 return false;
             }
             
-            if (!HasCommandsChanges())
+            if (HasCommandsChanges())
             {
                 await MessageBoxHelper("MainDialogueWindow", new MessageBoxOptions(
                     MessageBoxConstants.Error.Value, "Обнаружены операции с каталогами. Примите изменения или отмените их.",
